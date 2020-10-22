ad6bb121be5e48a06598bc8c2145313dd46c17cd
HolyZhang1120 <15803028040@163.com>
2020-10-22 10:09:27 +0800

    STM32F103-IAP升级

---
+++ README.md
@@ -0,0 +1,46 @@
+# stm32控制ESP8266获取天气api并且在led屏幕上显示
+
+------
+**使用：** 
+> * 在ATK_ESP8266文件夹下面的wifista.c第14行修改wifi的ssid号和密码。编译烧录后即可使用。
+> * 在ATK_ESP8266文件夹下面的weather.c中修改85行修改天气api。
+> * 在PICTURE文件夹下面的picture.c修改led显示的背景图片。
+> * 时间的显示可以使用stm32的实时时钟模块，然后串口授时；使用时间API获取北京时间。 
+
+
+**实验器材：**
+> * 战舰V3\精英STM32F103开发板
+> * ESP8266wifi模块
+	
+**实验目的：**
+> * 学习ATK_ESP8266高性能UART_WIFI模块的使用,掌握基本的TCP，UDP通信串口AT指令配置方法.
+> * 获取天气api,获得天气并且显示。
+
+**硬件资源：**
+	1,DS0(连接在PB5)
+	2,串口1(波特率:115200,PA9/PA10连接在板载USB转串口芯片CH340上面)
+	3,串口3(波特率:115200，PB10/PB11)
+	4,ALIENTEK 2.8/3.5/4.3/7寸TFTLCD模块(通过FSMC驱动,FSMC_NE4接LCD片选/A10接RS)
+	5,按键KEY0(PE4)/KEY1(PE3)/KEY_UP(PA0,也称之为WK_UP)
+	6,SD卡,通过SDIO(SDIO_D0~D4(PC8~PC11),SDIO_SCK(PC12),SDIO_CMD(PD2))连接 
+	7,W25Q128(SPI FLASH芯片,连接在SPI2上)
+	8,24C02
+	9,ATK_ESP8266 WIFI模块1个
+	10.3.5寸LCD屏幕
+	
+**连接方式：**
+	模块与带有无线网卡的电脑或其他wifi设备连接：采用wifi连接
+	模块与开发板连接（TTL串口方式）：
+	ATK-ESP8266 WIFI模块与精英板连接方式（通过4根杜邦线连接）：
+	ATK-ESP8266 WIFI模块    战舰V3\精英STM32开发板
+	   TXD<------------>PB11
+	   RXD<------------>PB10
+	   GND<------------>GND
+	   VCC<------------>3.3V\5V
+		
+**实验现象:**
+	通过api获取网络天气并且图文并茂显示，led上可以显示北京时间。
+	![运行结果][1]
+
+
+  [1]: https://github.com/wongnoubo/ESP8266-weather/blob/master/images/result.jpg
---
+++ USER/main.c
@@ -0,0 +1,102 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"      
+#include "malloc.h"
+#include "w25qxx.h"    
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "wdg.h"
+#include "stmflash.h"
+
+#define Flash_ADDR    0x1000		//存入flash起始位置
+#define Read_SIZE    2048
+
+typedef  void (*iapfun)(void);				//定义一个函数类型的参数.
+
+#define FLASH_APP1_ADDR		0x08009000  	//第一个应用程序起始地址(存放在FLASH)
+											//保留0X08000000~0X08008FFF的空间为IAP使用
+
+iapfun jump2app; 
+	
+int main(void)
+{	 
+	u32 Size;
+	u16 l,read_num;	
+	u8 pack,*crc32_file,write_buf2[4] = {0};
+	
+	u16 *iapbuf; 
+	
+	u32 fwaddr=FLASH_APP1_ADDR;//当前写入的地址
+	u32 readaddr=Flash_ADDR+4;//当前写入的地址
+	u16 t,tum;
+	u16 temp;
+	
+//	IWDG_Init(6,3125);
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	uart_init(115200);	 	//串口初始化为115200
+
+	W25QXX_Init();				//初始化W25Q128
+ 	my_mem_init(SRAMIN);		//初始化内部内存池
+//	IWDG_Feed();//喂狗
+	delay_ms(10);
+	printf("Init ok!");
+
+	while(1)
+	{
+		IWDG_Feed();//喂狗
+		iapbuf = mymalloc(SRAMIN,2300);
+		crc32_file = mymalloc(SRAMIN,2300);     //申请内存
+		W25QXX_Read(crc32_file,Flash_ADDR,4);
+		Size = crc32_file[1]*0x10000+crc32_file[2]*0x100+crc32_file[3];
+		if(crc32_file[0] == 0xF6)//判断文件CRC32
+		{	
+			printf("开始更新固件\r\n");
+			if(Size%Read_SIZE != 0) 
+				pack = Size/Read_SIZE + 1;
+			else pack = Size/Read_SIZE;
+			printf("pack：%d\r\n",pack);
+			for(l=0;l<pack;l++)
+			{
+				if(l == (pack-1))
+					read_num = Size - (Read_SIZE*l);
+				else read_num = Read_SIZE;
+				W25QXX_Read(crc32_file,readaddr,read_num);
+				readaddr += read_num;
+				for(t=0;t<read_num;t+=2)
+				{						    
+					temp=(u16)crc32_file[t+1]<<8;
+					temp+=(u16)crc32_file[t];	  
+					iapbuf[tum++]=temp;	    
+					if(tum==(1024))
+					{
+						tum=0;
+						STMFLASH_Write(fwaddr,iapbuf,1024);	
+						fwaddr+=2048;//偏移2048  16=2*8.所以要乘以2.
+					}
+				}	
+				if(tum)STMFLASH_Write(fwaddr,iapbuf,tum);//将最后的一些内容字节写进去. 
+				delay_ms(10);	
+			}
+			W25QXX_Write((u8*)write_buf2,Flash_ADDR,4);
+			printf("固件更新完成!\r\n");	
+		}	
+		myfree(SRAMIN,iapbuf);
+		myfree(SRAMIN,crc32_file);   //释放内存	
+		printf("有固件\r\n");
+		if(((*(vu32*)(FLASH_APP1_ADDR+4))&0xFF000000)==0x08000000)//判断是否为0X08XXXXXX.
+		{	
+			if(((*(vu32*)FLASH_APP1_ADDR)&0x2FFE0000)==0x20000000)	//检查栈顶地址是否合法.
+			{ 
+				jump2app=(iapfun)*(vu32*)(FLASH_APP1_ADDR+4);		//用户代码区第二个字为程序开始地址(复位地址)		
+				MSR_MSP(*(vu32*)FLASH_APP1_ADDR);					//初始化APP堆栈指针(用户代码区的第一个字用于存放栈顶地址)
+				jump2app();									//跳转到APP.
+			}	
+		}			
+	}
+}
+
+
+
+
